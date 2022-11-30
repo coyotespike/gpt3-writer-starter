83542a4506aa8d7b73aeebbf2917c3dd8982d326
Timothy Roy <timothy.roy@protonmail.ch>
2022-11-30 10:27:40 -0600

    feat: add animated Christmas card

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# buildspace 
-### Welcome 👋
-This is the starter template for the [build your own AI writing assistant w/ GPT-3](https://buildspace.so/builds/ai-writer) project.
+# Card Generator
+
+## Basics
+- Login?
+- Start with an example card with picture and text
+- Then "Generate"
+- How should UX work here?
+-- Start simple. White Christmas line drawing background. Example card.
+https://image.lexica.art/md/eb9e86f8-bb09-4273-a058-bf8de19a10e6
+-- Have GPT generate copy. This year, get some help sending holiday cards! Have an Elf write it for you!
+--- Click to generate your own! We will mail a physical card for you - or send an e-card
+-- Inside, a small form. Your name, recipient's name. Generate.
+-- Click to edit text or choose a different picture.
+
+## Later
+- Choose card style
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import { useState } from "react";
 import Image from "next/image";
 import buildspaceLogo from "../assets/buildspace-logo.png";
+import { Card } from "../components/Card";
 
 const Home = () => {
   const [userInput, setUserInput] = useState("");
   const [isGenerating, setIsGenerating] = useState(false);
   const [apiOutput, setApiOutput] = useState("");
+
+  const [isOpened, setIsOpened] = useState(false);
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
@@ -34,68 +37,7 @@ const Home = () => {
   };
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>Birthday Card Writer</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>Give us a name and an age and we'll do the rest!</h2>
-          </div>
-        </div>
-        <div>
-          <textarea
-            className="prompt-box"
-            placeholder="start typing here"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
-          <div className="prompt-buttons">
-            <a
-              className={
-                isGenerating ? "generate-button loading" : "generate-button"
-              }
-              onClick={callGenerateEndpoint}
-            >
-              <div className="generate">
-                {isGenerating ? (
-                  <span className="loader"></span>
-                ) : (
-                  <p>Generate</p>
-                )}
-              </div>
-            </a>
-          </div>
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
-                </div>
-              </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
+      <Card />
     </div>
   );
 };
