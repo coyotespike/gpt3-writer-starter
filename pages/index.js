b808de2238966b2341b560e00990cd1827264fc9
Timothy Roy <timothy.roy@protonmail.ch>
2022-11-30 21:04:11 -0600

    refactor: remove unnecessary items from landing page Home

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,19 @@
-import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState } from "react";
-import { BackgroundImage, Card, ImageGallery } from "../components";
-import { Button, Grid, Item, Stack } from "@mui/material";
-import emoji from "node-emoji";
-
-const Emoji = (props) => {
-  const symbol = emoji.get(props.name);
-
-  return (
-    <span
-      className="emoji"
-      role="img"
-      aria-label={props.name}
-      aria-hidden={false}
-    >
-      {symbol}
-    </span>
-  );
-};
+import { Button, Stack } from "@mui/material";
+import { Card, Emoji } from "components";
 
 const Home = () => {
-  const [userInput, setUserInput] = useState("");
-  const [isGenerating, setIsGenerating] = useState(false);
-  const [apiOutput, setApiOutput] = useState("");
-
   const [isOpened, setIsOpened] = useState(false);
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    async function fetchImages() {
-      const response = await fetch("/api/lexica");
-      const data = await response.json();
-      setImages(data.images);
-    }
-    // fetchImages();
-  }, []);
 
-  const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
-
-    console.log("Calling OpenAI...");
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userInput }),
-    });
-
-    const data = await response.json();
-
-    const { output } = data;
-    console.log("OpenAI replied...", output);
-
-    setApiOutput(`${output}`);
-    setIsGenerating(false);
-  };
-
-  const onUserChangedText = (event) => {
-    setUserInput(event.target.value);
-  };
   return (
-    <div>
-      <BackgroundImage />
-      <Stack justifyContent="center" alignItems="center" spacing={10}>
-        <div className="is-hidden">for vertical spacing</div>
-        <div onClick={() => setIsOpened((isOpened) => !isOpened)}>
-          <Card isOpened={isOpened} />
-        </div>
+    <Stack justifyContent="center" alignItems="center" spacing={10}>
+      <div className="is-hidden">for vertical spacing</div>
+      <div onClick={() => setIsOpened((isOpened) => !isOpened)}>
+        <Card isOpened={isOpened} />
+      </div>
+
+      <Link href="/artwork" style={{ textDecoration: "none" }}>
         <Button
           variant="contained"
           color="success"
@@ -75,10 +22,8 @@ const Home = () => {
         >
           Get started!
         </Button>
-        {/* {images && <ImageGallery images={images} />} */}
-        {/* <ImageGallery /> */}
-      </Stack>
-    </div>
+      </Link>
+    </Stack>
   );
 };
 
